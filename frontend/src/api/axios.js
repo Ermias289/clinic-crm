bb40c1765eb8401b64261e051b18a95876301aea
Jermiah Molla <ermiasmolla00@gmail.com>
2025-12-27 17:57:12 +0300

    feat: connected web app with the deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.100.17.164:5246", // Your .NET backend URL (local IP)
+  baseURL: "https://crmgate.nexabusinessgroup.com", // Your .NET backend URL (local IP)
   headers: {
     "Content-Type": "application/json",
   },
