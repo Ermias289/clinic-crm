5c8921c99df7c655a7f1f905444e596df2edbc02
mamee13 <mamaruyirga1394@gmail.com>
2026-01-14 21:55:13 +0300

    Configure Lucid Dental app ID: com.luciddental.clinic

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mobile_app"
+    namespace = "com.luciddental.clinic"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobile_app"
+        // Lucid Dental unique Application ID
+        applicationId = "com.luciddental.clinic"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
