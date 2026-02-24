a459d845a7e735f0d85ae644ef6c03cd167ca36c
mamee13 <mamaruyirga1394@gmail.com>
2026-02-24 09:56:02 +0300

    feat(register): Add middle name field to user registration form

--- lib/presentation/controllers/register_controller.dart
+++ lib/presentation/controllers/register_controller.dart
@@ -12,6 +12,7 @@ class RegisterController extends GetxController {
   final usernameController = TextEditingController();
   final fullnameController = TextEditingController();
   final fNameController = TextEditingController();
+  final mNameController = TextEditingController();
   final lNameController = TextEditingController();
   final emailController = TextEditingController();
   final phoneController = TextEditingController();
@@ -101,13 +102,14 @@ class RegisterController extends GetxController {
         userRoleId = await registerUseCase.getPatientRoleId();
       }
 
-      final fullname = '${fNameController.text} ${lNameController.text}';
+      final fullname =
+          '${fNameController.text} ${mNameController.text.isNotEmpty ? '${mNameController.text} ' : ''}${lNameController.text}';
 
       final request = RegisterRequestModel(
         username: usernameController.text.trim(),
         fullname: fullname.trim(),
         fName: fNameController.text.trim(),
-        mName: "", // Empty for now as removed from UI
+        mName: mNameController.text.trim(),
         lName: lNameController.text.trim(),
         email: emailController.text.trim(),
         phoneNumber: phoneController.text.trim(),
